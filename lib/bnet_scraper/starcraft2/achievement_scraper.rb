8b7415da9ed3a22f485a17192976fbf53bd57fdf
Andrew Nordman <cadwallion@gmail.com>
2012-03-12 14:19:34 -0500

    Refactors scrape, adds output
    AchievementScraper#scrape has turned into a delegator to
    scrape_progress, scrape_showcase, scrape_recent, and get_response.
    
    AchievementScraper#get_response is what handles HTML retrieval to
    allow easier testing of the sub-scrapes.

--- lib/bnet_scraper/starcraft2/achievement_scraper.rb
+++ lib/bnet_scraper/starcraft2/achievement_scraper.rb
@@ -1,7 +1,7 @@
 module BnetScraper
   module Starcraft2
     class AchievementScraper
-      attr_reader :region, :bnet_id, :account, :bnet_index, :recent, :progress, :showcase
+      attr_reader :region, :bnet_id, :account, :bnet_index, :recent, :progress, :showcase, :response
       def initialize options = {}
         if options[:url]
           extracted_data = options[:url].match(/http:\/\/(.+)\/sc2\/(.+)\/profile\/(.+)\/(\d{1})\/(.+)\/achievements\//)
@@ -45,26 +45,33 @@ module BnetScraper
       end
 
       def scrape
-        response = Nokogiri::HTML(open(achievement_url)) 
-        @showcase = response.css("#showcase-module .progress-tile").map do |achievement|
-          hsh = { title: achievement.css('.tooltip-title').inner_text.strip }
-          hsh[:description] = achievement.css('div').inner_text.gsub(hsh[:title], '').strip
-          hsh
-        end
-        
+        get_response
+        scrape_recent
+        scrape_progress
+        scrape_showcase
+      end
+
+      def get_response
+        @response = Nokogiri::HTML(open(achievement_url)) 
+      end
+
+      def scrape_recent
         @recent = []
         6.times do |num|
           achievement = {}
           div = response.css("#achv-recent-#{num}")
           if div
-            achievement[:title] = div.css("div").inner_text.strip
+            achievement[:title] = div.css("div > div").inner_text.strip
             achievement[:description] = div.inner_text.gsub(achievement[:title], '').strip
-            achievement[:earned] = response.css("#recent-achievements a:nth(#{num}) span:nth(1)").inner_text
+            achievement[:earned] = response.css("#recent-achievements span")[(num*3)+1].inner_text
 
             @recent << achievement
           end
         end
+        @recent
+      end
 
+      def scrape_progress
         progress_ach = response.css("#progress-module .achievements-progress:nth(2) span")
         @progress = {
           liberty_campaign: progress_ach[0].inner_text,
@@ -72,6 +79,23 @@ module BnetScraper
           custom_game:      progress_ach[2].inner_text,
           cooperative:      progress_ach[3].inner_text,
           quick_match:      progress_ach[4].inner_text,
+        }
+      end
+
+      def scrape_showcase
+        @showcase = response.css("#showcase-module .progress-tile").map do |achievement|
+          hsh = { title: achievement.css('.tooltip-title').inner_text.strip }
+          hsh[:description] = achievement.css('div').inner_text.gsub(hsh[:title], '').strip
+          hsh
+        end
+        @showcase
+      end
+
+      def output
+        {
+          recent: @recent,
+          progress: @progress,
+          showcase: @showcase
         }
       end
     end
--- spec/starcraft2/achievement_scraper_spec.rb
+++ spec/starcraft2/achievement_scraper_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe BnetScraper::Starcraft2::AchievementScraper do
+  let(:url) { 'http://us.battle.net/sc2/en/profile/2377239/1/Demon/achievements/' }
+  subject { BnetScraper::Starcraft2::AchievementScraper.new(url: url) }
+
   describe '#initialize' do
     context 'with url parameter passed' do
-      subject { BnetScraper::Starcraft2::AchievementScraper.new(url: 'http://us.battle.net/sc2/en/profile/2377239/1/Demon/achievements/') }
-
       it 'should extract bnet_id from the URL' do
         subject.bnet_id.should == '2377239' 
       end
@@ -51,19 +52,60 @@ describe BnetScraper::Starcraft2::AchievementScraper do
     end
   end
 
+  describe '#get_response' do
+    it 'should get the HTML response to be scraped' do
+      subject.response.should be_nil
+      subject.get_response
+      subject.response.should_not be_nil
+    end
+  end
+
   describe '#scrape' do
-    subject { BnetScraper::Starcraft2::AchievementScraper.new(url: 'http://us.battle.net/sc2/en/profile/2377239/1/Demon/achievements/') }
-    before :each do
+    it 'should call get_response and trigger scraper methods' do
+      subject.should_receive(:get_response)
+      subject.should_receive(:scrape_progress)
+      subject.should_receive(:scrape_recent)
+      subject.should_receive(:scrape_showcase)
       subject.scrape
+    end
+  end
+
+  describe '#scrape_showcase' do
+    before :each do
+      subject.get_response
+      subject.scrape_showcase
     end
 
     it 'should set the showcase' do
       subject.showcase.should have(5).achievements
     end
+  end
 
-    it 'should set the recently earned' do
-      subject.recent.should have(6).achievements
+  describe '#scrape_recent' do
+    before :each do
+      subject.get_response
+      subject.scrape_recent
     end
+
+    it 'should have the title of the achievement' do
+      subject.recent[0][:title].should == 'Blink of an Eye'  
+    end
+
+    it 'should have the description of the achievement' do
+      # this is a cop-out because the string contains UTF-8. Please fix this. - Cad
+      subject.recent[0][:description].should be_a String 
+    end
+
+    it 'should have the date the achievement was earned' do
+      subject.recent[0][:earned].should == '3/5/2012' 
+    end
+  end
+
+  describe '#scrape_progress' do
+    before :each do
+      subject.get_response
+      subject.scrape_progress
+    end 
 
     it 'should set the liberty campaign progress' do
       subject.progress[:liberty_campaign].should == '1580'
@@ -83,6 +125,18 @@ describe BnetScraper::Starcraft2::AchievementScraper do
 
     it 'should set the quick match progress' do
       subject.progress[:quick_match].should == '170'
+    end
+  end
+
+  describe '#output' do
+    it 'should return the scraped data when scrape has been called' do
+      subject.scrape
+      expected = {
+        recent: subject.recent,
+        showcase: subject.showcase,
+        progress: subject.progress
+      }
+      subject.output.should == expected
     end
   end
 end
