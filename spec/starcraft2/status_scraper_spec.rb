cc1e76242c4ed0d217271fa867d1cc1e7a85efe7
Andrew Nordman <cadwallion@gmail.com>
2013-02-20 00:34:56 -0600

    Status fixtures

--- spec/starcraft2/status_scraper_spec.rb
+++ spec/starcraft2/status_scraper_spec.rb
@@ -3,14 +3,21 @@ require 'spec_helper'
 describe BnetScraper::Starcraft2::Status do
   describe 'Each supported region' do
     it 'should be online' do
-      BnetScraper::Starcraft2::Status.na.should == 'Online'
-      BnetScraper::Starcraft2::Status.eu.should == 'Online'
-      BnetScraper::Starcraft2::Status.sea.should == 'Online'
-      BnetScraper::Starcraft2::Status.fea.should == 'Online'
+      VCR.use_cassette('realm_status') do
+        BnetScraper::Starcraft2::Status.na.should == 'Online'
+        BnetScraper::Starcraft2::Status.eu.should == 'Offline'
+        BnetScraper::Starcraft2::Status.sea.should == 'Online'
+        BnetScraper::Starcraft2::Status.fea.should == 'Online'
+      end
     end
   end
 
+
   describe 'China' do
-    BnetScraper::Starcraft2::Status.cn.should == nil
+    it 'should be nil' do
+      VCR.use_cassette('realm_status') do
+        BnetScraper::Starcraft2::Status.cn.should == nil
+      end
+    end
   end
 end
