458fe38a258f01bd0f0c69764398aa32c40c98b7
Andrew Nordman <cadwallion@gmail.com>
2013-07-24 09:03:24 -0500

    Adds account back to profile scraping results

--- lib/bnet_scraper/starcraft2/profile_scraper.rb
+++ lib/bnet_scraper/starcraft2/profile_scraper.rb
@@ -4,32 +4,14 @@ require 'bnet_scraper/starcraft2/portrait'
 module BnetScraper
   module Starcraft2
     # This pulls basic profile information for an account, as well as an array of league URLs.  This is a good starting
-    # point for league scraping as it provides the league URLs necessary to do supplemental scraping.
+    # point for league scraping as it provides the league URLs necessary to do supplemental scraping. The `scrape`
+    # command returns an instance of `BnetScraper::Starcraft2::Profile` containing all relevant profile information
     #
     #   scraper = BnetScraper::Starcraft2::ProfileScraper.new(url: 'http://us.battle.net/sc2/en/profile/2377239/1/Demon/')
-    #   scraper.scrape
-    #   # => {
-    #     bnet_id: '2377239',
-    #     account: 'Demon',
-    #     bnet_index: 1,
-    #     race: 'Protoss',
-    #     wins: '684',
-    #     achievement_points: '3630',
-    #     current_solo_league: 'Not Yet Ranked',
-    #     highest_solo_league: 'Platinum',
-    #     current_team_league: 'Not Yet Ranked',
-    #     highest_team_league: 'Diamond',
-    #     career_games: '1568',
-    #     games_this_season: '0',
-    #     most_played: '4v4',
-    #     leagues: [
-    #       {
-    #         name: "1v1 Platinum Rank 95",
-    #         id:   "96905",
-    #         href: "http://us.battle.net/sc2/en/profile/2377239/1/Demon/ladder/96905#current-rank"
-    #       }
-    #     ]
-    #   }
+    #   profile = scraper.scrape
+    #   profile.class.name # => 'BnetScraper::Starcraft2::Profile'
+    #   profile.race # => 'Protoss'
+    #   profile.career_games # => '1568'
     class ProfileScraper < BaseScraper
       attr_reader :achievement_points, :career_games, :leagues, :games_this_season, 
         :highest_solo_league, :current_solo_league, :highest_team_league,
@@ -39,7 +21,7 @@ module BnetScraper
       def initialize options = {}
         super
         @leagues = []
-        @profile ||= Profile.new url: profile_url
+        @profile ||= Profile.new url: profile_url, account: account
       end
 
       def scrape
