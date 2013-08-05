60bdc9801bceac4fe671bed5ae459f63cd58b557
Buddy Magsipoc <keikun17@gmail.com>
2013-08-05 15:17:09 +0800

    Reload cassettes using webmock.

--- spec/starcraft2/grandmaster_scraper_spec.rb
+++ spec/starcraft2/grandmaster_scraper_spec.rb
@@ -9,7 +9,7 @@ describe BnetScraper::Starcraft2::GrandmasterScraper do
         scraper.scrape
       end
     end
-    it { should have(200).players }
+    it { should have_at_most(200).players }
 
     context 'player statistics' do
       let(:player) { scraper.players[0] }
@@ -19,23 +19,23 @@ describe BnetScraper::Starcraft2::GrandmasterScraper do
       end
 
       it 'should have a race' do
-        player[:race].should == 'zerg'
+        player[:race].should == 'protoss'
       end
 
       it 'should have a name' do
-        player[:name].should == '[Qntc] QuanticHyuN'
+        player[:name].should == 'Aiur'
       end
 
       it 'should have points' do
-        player[:points].should == '953'
+        player[:points].should == '2045'
       end
 
       it 'should have wins' do
-        player[:wins].should == '71'
+        player[:wins].should == '137'
       end
 
       it 'should have losses' do
-        player[:losses].should == '11'
+        player[:losses].should == '61'
       end
     end
   end
--- spec/starcraft2/league_spec.rb
+++ spec/starcraft2/league_spec.rb
@@ -1,31 +1,31 @@
 require 'spec_helper'
 
 describe BnetScraper::Starcraft2::League do
-  let(:href) { 'http://us.battle.net/sc2/en/profile/2377239/1/Demon/ladder/134659' }
+  let(:href) { 'http://us.battle.net/sc2/en/profile/2377239/1/Demon/ladder/155488' }
   let(:league) { BnetScraper::Starcraft2::League.new href: href }
   subject { league }
 
   it 'scrapes if name is not set' do
     VCR.use_cassette('demon_leagues') do
-      league.name.should == 'Kalathi Echo'
+      league.name.should == 'Dropship Victor'
     end
   end
 
   it 'scrapes if season is not set' do
     VCR.use_cassette('demon_leagues') do
-      league.season.should == '2013 Season 1'
+      league.season.should == '2013 Season 4'
     end
   end
 
   it 'scrapes if division is not set' do
     VCR.use_cassette('demon_leagues') do
-      league.division.should == 'Platinum'
+      league.division.should == 'Silver'
     end
   end
 
   it 'scrapes if size is not set' do
     VCR.use_cassette('demon_leagues') do
-      league.size.should == '1v1'
+      league.size.should == '2v2'
     end
   end
 
